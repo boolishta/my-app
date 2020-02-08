78634e099cf315bf2b8c13396d514fd1ac58247c
boolishta <batr.fly@mail.ru>
2020-02-08 21:38:09 +0300

    add app.js

---
+++ src/api/api.js
@@ -0,0 +1,35 @@
+import Axios from "axios";
+
+const instance = Axios.create({
+  withCredentials: true,
+  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+  headers: {
+    "API-KEY": "6946ff38-638a-4018-ac8b-6ecca0f18517"
+  }
+})
+
+export const usersAPI = {
+  getUsers(currentPage = 1, pagesSize = 10)  { //запрос на сервер на всех юзеров, в параметры передаем то что раньше приходило через this.props
+    return instance.get(`users?page=${currentPage}&count=${pagesSize}`)
+      .then(Response => {
+        return Response.data; //передаем в response не все а только data
+    });
+  },
+  getUnfollow(u) {
+    return instance.delete(`follow/${u.id}`).then(Response => {
+      return Response.data
+    })
+  },
+  getFollow(u) {
+    return instance.post(`follow/${u.id}`).then(Response => {
+      return Response.data
+    })
+  },
+  getAuthLogin() { //если залогинин то сетаем данные
+    return instance.get(`auth/me`)
+  },
+  getAuthMe(userId) { //получаем данные для вывода на profile
+    return instance.get(`profile/` + userId)
+  }
+}
+
