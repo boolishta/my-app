91bc637d797f6481f753ef43513d4f308e8da932
boolishta <batr.fly@mail.ru>
2020-02-16 22:49:53 +0300

    87 add React.memo
    Что бы компонента постоянно не рендерилась, оборачиваем ее в Reacn.memo
    Тогда она рендерится только при изменении props или state
    
    Если компонента классовая то extend Pure.Component
    Или применяем метод shouldComponentUpdate

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { maxLengthCreator, required } from '../../../utils/validators/validator';
+import { Textarea } from '../../common/FormsControls/FormsControls';
 import style from './MyPosts.module.css';
 import Post from './Post/Post';
-import { Textarea } from '../../common/FormsControls/FormsControls';
 
 
 const maxLength10 = maxLengthCreator(10); // если > 10 символов то ошибка
 
-const MyPosts = (props) => {
+const MyPosts = React.memo( (props) => { //React.memo оборачивает компоненту и перерисовывает ее только когда были изменены пропсы или state
 
-  let postsElements = props.posts.map( e => <Post message={e.message} likesCount={e.likesCount}/>);
-  const onAddPost = (values) => { props.addPost(values.newPostText); }; //диспатчим текст из формы
-  return (
-    <div className = { style.posts }>
-      <p>My posts:</p>
-      <AddNewPostFormRedux onSubmit={onAddPost}/>
-      <div className = { style.allPosts }> { postsElements } </div>
-    </div>
-  );
-}
+  // shouldComponentUpdate(nextProps, nextState) { //применяется в классовой компоненте, отрисовывается если пришли новые props или state
+  //   return nextProps !== this.props || nextState !== this.state; //вернем true если props или state изменился
+  // }
+
+    let postsElements = props.posts.map( e => <Post message={e.message} likesCount={e.likesCount}/>);
+    const onAddPost = (values) => { props.addPost(values.newPostText); }; //диспатчим текст из формы
+    return (
+      <div className = { style.posts }>
+        <p>My posts:</p>
+        <AddNewPostFormRedux onSubmit={onAddPost}/>
+        <div className = { style.allPosts }> { postsElements } </div>
+      </div>
+    );
+});
 
 const AddNewPostForm = (props) => {
   return (
