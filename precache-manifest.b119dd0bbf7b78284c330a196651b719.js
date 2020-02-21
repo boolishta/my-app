f8407b156dfd43e70d57123b7b5f8f3d6cfb90b7
boolishta <batr.fly@mail.ru>
2020-02-21 14:51:22 +0300

    Updates

---
+++ precache-manifest.b119dd0bbf7b78284c330a196651b719.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d4c223b1e1257d501f6b9e4fe26b347",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "d9486606fd0b21ac68b1",
+    "url": "/my-app/static/css/3.d0c29de8.chunk.css"
+  },
+  {
+    "revision": "a730b16e116f82b5d099",
+    "url": "/my-app/static/css/4.2c8b87c2.chunk.css"
+  },
+  {
+    "revision": "085490a118d48201e02a",
+    "url": "/my-app/static/css/main.6e0b85ce.chunk.css"
+  },
+  {
+    "revision": "9f05cd27af8a22bb8e1e",
+    "url": "/my-app/static/js/2.e50b5067.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/my-app/static/js/2.e50b5067.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d9486606fd0b21ac68b1",
+    "url": "/my-app/static/js/3.d98a6dfb.chunk.js"
+  },
+  {
+    "revision": "a730b16e116f82b5d099",
+    "url": "/my-app/static/js/4.158c7fcf.chunk.js"
+  },
+  {
+    "revision": "085490a118d48201e02a",
+    "url": "/my-app/static/js/main.951a182e.chunk.js"
+  },
+  {
+    "revision": "1e70db4cc6111479b726",
+    "url": "/my-app/static/js/runtime-main.1d05f1aa.js"
+  },
+  {
+    "revision": "edc625f7bb114ac647c21b6b8c80b7be",
+    "url": "/my-app/static/media/spinner.edc625f7.svg"
+  }
+]);
